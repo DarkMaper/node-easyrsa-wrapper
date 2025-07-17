5ca7f4a9a7f0ff4322388ce0324acc183d78233b
DarkMaper <darkmaper@gmail.com>
2025-07-17 17:13:39 +0200

    Update jest config file to typescript and added jest rules to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,16 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import globals from '@jest/globals';
 
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommended,
     eslintConfigPrettier,
-    globals.jest
+    {
+        files: ['test/**/*'],
+        plugins: ['jest'],
+        env: {
+            "jest/globals": true 
+        }
+    }
 )
