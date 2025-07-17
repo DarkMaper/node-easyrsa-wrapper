14a1e9d2ee62d94fa8be8445733b2b2ed305e8d9
DarkMaper <darkmaper@gmail.com>
2025-07-17 16:55:29 +0200

    Install @jest/globals for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from '@jest/globals';
 
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommended,
-    eslintConfigPrettier
+    eslintConfigPrettier,
+    globals.jest
 )
